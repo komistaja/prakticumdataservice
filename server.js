1deac85cfd60b00b7d7171c3a3d04b682ae8918e
komistaja <kolistelija@gmail.com>
2016-12-16 12:24:00 +0200

    Added ticket model module

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const app = express();
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 
+const ticketModel = require('./ticketmodel');
+
 mongoose.connect('mongodb://localhost/test');
 
 db.on('error', console.error.bind(console, 'connection error:'));
+
 db.once('open', function() {
   console.log('database connected');
+  
+  var ticket = mongoose.model('Ticket', ticketModel.ticketSchema);
 });
 
 app.listen(process.env.PORT || 3000, function () {
