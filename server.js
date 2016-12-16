dac956462a02dfec172d08b0528aed015f1936be
komistaja <kolistelija@gmail.com>
2016-12-16 09:54:21 +0200

    Added server files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('server up');
+});
+
+app.use(express.static('public'));
+
+app.get('/zones', (req, res) => {
+  
+    res.send('benis');
+    console.log('page refresh');
+});
