260aa5b845b11360957a080e7d6e3fe522e7c750
komistaja <kolistelija@gmail.com>
2016-12-16 11:38:17 +0200

    Started work on databaseconnection

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+mongoose.connect('mongodb://localhost/test');
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('database connected');
+});
+
 app.listen(process.env.PORT || 3000, function () {
     console.log('server up');
 });
