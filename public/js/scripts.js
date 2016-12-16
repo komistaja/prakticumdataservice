578b3a9edec761cb190b58604b711bee47daf0fd
komistaja <kolistelija@gmail.com>
2016-12-16 13:58:18 +0200

    Added .post example for collaborator

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,11 @@
 $(function(){
-  $('button').click(function(){
-    $.post('/zones', { fname: 'Jorma', lname: 'Penttinen' },
+  $('#button').click(function(){  
+    var benis = $('#text').val();
+    $.post('/zones', { text: benis },
       
       function(data, status){
        console.log(data);
+       alert('Check console!');
       });
     });
 });
