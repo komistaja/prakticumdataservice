c88ea2a98482a939dfc59be0a21c2388aa84f883
komistaja <kolistelija@gmail.com>
2017-01-13 09:39:58 +0200

    Functionality testing and minor changes on front
    Added search response table to main page.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,6 +2,7 @@ $(function(){
   
   //Add data to database
   $('#button').click(function(){
+    //check if required fields are empty
     if($('#fname').val() && $('#lname').val() && $('#email').val() && $('#tel').val() && $('#service').val()) {
       var fname = $('#fname').val();
       var lname = $('#lname').val();
@@ -10,7 +11,8 @@ $(function(){
       var date = new Date();
       var service = $('#service').val();
       var comments = $('#comments').val();
-
+      
+      //post data to server/database
       $.post('/add', {
           fname: fname,
           lname: lname,
@@ -29,7 +31,7 @@ $(function(){
       }
     });
   
-  //Get data
+  //Get searchdata
   $('#button2').click(function() {
     var email = $('#email').val();
     $.get('/search',{ email: email }, function(data, status) {
@@ -41,6 +43,24 @@ $(function(){
       $('#service').val(data[0].service);
       $('#comments').val(data[0].comments);
       $('#id').val(data[0]._id);
+    });
+  });
+  
+  //search
+  $('#button2').click(function() {
+    var email = $('#email').val();
+    var id = $('#id').val();
+
+    $.get('/workersearch',{ email: email, id: id }) 
+      .done(function(data) {
+      console.log(data);
+      
+      //Create result table
+      $('#restable').empty();
+      $('#restable').append('<tr><th>Id</th><th>Email</th><th>Service</th><th>Comments</th></tr>');
+      for(i = 0; i < data.length; i++) {
+        $('#restable').append("<tr><td>" + data[i]._id + "</td><td>" + data[i].email + "</td><td>" + data[i].service + "</td><td>" + data[i].comments + "</td></tr>");
+      }
     });
   });
   
